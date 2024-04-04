3b5c52fd6976ab5c60bb620343ffdc496b78e84f
Guilherme Henrique <guilhermeportosantos1@gmail.com>
2024-04-03 21:24:52 -0300

    States and fakeData

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonIcon extends ComponentProps<'button'> {
   transparent?: true
@@ -7,10 +8,18 @@ interface ButtonIcon extends ComponentProps<'button'> {
 export function ButtonIcon({transparent, ...props}: ButtonIcon) {
 
   return (
-    <button {...props} className= {transparent
-      ? 'bg-black/20 border-white/10 rounded-md p-1.5'
-      : 'bg-white/10 border-white/10 rounded-md p-1.5'
-      }
+    <button {...props}
+    //  className= {transparent
+    //   ? 'bg-black/20 border-white/10 rounded-md p-1.5'
+    //   : 'bg-white/10 border-white/10 rounded-md p-1.5'
+    //   }
+
+    className={twMerge(
+      'border border-white/10 rounded-md p-1.5',
+      transparent ? 'bg-black/20' : 'bg-white/10',
+      props.disabled ? 'opacity-50' : null
+      
+    )}
     />
   )
 }
--- src/components/attendeeList.tsx
+++ src/components/attendeeList.tsx
@@ -3,15 +3,47 @@ import { ButtonIcon } from './ButtonIcon'
 import { Table } from './table/Table'
 import { TableHeader } from './table/TableHeader'
 import { TableCell } from './table/TableCell'
+import { attendees } from '../data/attendees'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/pt-br'
+import { ChangeEvent, useState } from 'react'
+
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
 
 export function AttendeeList(){
+    const [search, setSearch] = useState('')
+    const[page, setPage] = useState(1)
+    const totalPages = Math.ceil(attendees.length / 10)
+
+    function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>){
+        setSearch(event.target.value)
+    }
+
+    function goToNextPage(){
+        setPage(page + 1)
+    }
+
+    function goToPrevPage(){
+        setPage(page - 1)
+    }
+
+    function goToFirstPage(){
+        setPage(1)
+    }
+
+    function goToLastPage(){
+        setPage(totalPages)
+    }
+
     return (
         <div className='flex flex-col gap-4'>
             <div className="flex gap-3 items-center">
                 <h1 className="text-2xl font-bold">participantes</h1>
                 <div className="gap-3 px-3 w-72 py-1.5 border border-white/10 bg-transparent rounded-lg flex items-center">
                     <Search className='size-4 text-emerald-300'/>
-                    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar Participantes..." />
+                    <input onChange={onSearchInputChanged}  className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar Participantes..." />
                 </div>
             </div>
 
@@ -31,21 +63,22 @@ export function AttendeeList(){
                     </thead>
 
                     <tbody>
-                        {Array.from({ length: 6}).map((_, i) => {
+                        {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
                             return (
-                                <tr key={i} className='border-b border-white/10 hover:bg-white/5 '>
+                                <tr key={attendee.id} className='border-b border-white/10 hover:bg-white/5 '>
                                     <TableCell>
                                         <input type="checkbox" className='siz-4 bg-black/20 rounded border-white/10'/>
                                     </TableCell>
-                                    <TableCell>1234</TableCell>
+                                    <TableCell>{attendee.id}</TableCell>
                                     <TableCell>
                                         <div className="flex flex-col gap-1">
-                                            <span className='font-semibold text-white'>Guilherme Henrique Porto</span>
-                                            <span>guiporto@gmail.com</span>
+                                            <span className='font-semibold text-white'>{attendee.name}</span>
+                                            <span>{attendee.email}</span>
                                         </div>
                                     </TableCell>
-                                    <TableCell>7 Dias atras</TableCell>
-                                    <TableCell>3 Dias atras</TableCell>
+                                    <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                                    <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+
                                     <TableCell>
                                         <ButtonIcon transparent>
                                             <MoreHorizontal className='size-4'/>
@@ -58,26 +91,26 @@ export function AttendeeList(){
                     <tfoot>
                         <tr>
                             <TableCell className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>
-                                Mostrando 10 de 244
+                                Mostrando 10 de {attendees.length} itens
                             </TableCell>
                             <TableCell className='text-right' colSpan={3}>
                                 <div className='inline-flex gap-8 items-center'>
-                                    <span>Pagina 1 de 15</span>
+                                    <span>Pagina {page} de {totalPages}</span>
 
                                     <div className='flex gap-1.5'>
-                                        <ButtonIcon>
+                                        <ButtonIcon onClick={goToFirstPage} disabled={page === 1}>
                                             <ChevronsLeft className='size-4'/>
                                         </ButtonIcon>
                                         
-                                        <ButtonIcon>
+                                        <ButtonIcon onClick={goToPrevPage} disabled={page == 1}>
                                             <ChevronLeft className='size-4'/>
                                         </ButtonIcon>
                                         
-                                        <ButtonIcon>
+                                        <ButtonIcon onClick={goToNextPage} disabled={page === totalPages}>
                                             <ChevronRight className='size-4'/>
                                         </ButtonIcon>
 
-                                        <ButtonIcon>
+                                        <ButtonIcon onClick={goToLastPage} disabled={page === totalPages}>
                                             <ChevronsRight className='size-4'/>
                                         </ButtonIcon>
                                     </div>
